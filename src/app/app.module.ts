5867387449a3e94eb1c58ffb2c74d1a381543093
Amritpal Singh <sohal@Sohal-MacBook-Pro.local>
2020-09-17 01:40:51 +0530

    chat-app-commit-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//routing
 import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChatModule,
+    UserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '*', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ChatBoxComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'chat', component: ChatBoxComponent }
+    ])
   ]
 })
 export class ChatModule { }
